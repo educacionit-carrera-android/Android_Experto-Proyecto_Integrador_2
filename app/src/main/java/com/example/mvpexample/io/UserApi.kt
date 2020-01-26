120e1c5839832c61b29ba2c0a8bdfa39a5ac297e
carrera.android <carrera.android@educacionit.com>
2020-01-26 01:10:56 -0300

    Add user parameter to model layer

--- app/src/main/java/com/example/mvpexample/io/UserApi.kt
+++ app/src/main/java/com/example/mvpexample/io/UserApi.kt
@@ -1,12 +1,22 @@
 package com.example.mvpexample.io
 
+import com.example.mvpexample.data.User
 import com.example.mvpexample.data.UserResponse
 import io.reactivex.Single
 
-class UserApi {
+class UserApiImp : UserApi {
 
-    fun login(): Single<UserResponse> = Single.just(UserResponse("Jose", "Argento"))
+    override fun login(user: User): Single<UserResponse> =
+        Single.just(UserResponse("Jose", "Argento"))
 
-    fun loginError(): Single<UserResponse> = Single.error(Throwable("Datos incorrectos"))
+    override fun loginError(): Single<UserResponse> = Single.error(Throwable("Datos incorrectos"))
+
+}
+
+interface UserApi {
+
+    fun login(user: User): Single<UserResponse>
+
+    fun loginError(): Single<UserResponse>
 
 }
--- app/src/main/java/com/example/mvpexample/model/LoginRepositoryImp.kt
+++ app/src/main/java/com/example/mvpexample/model/LoginRepositoryImp.kt
@@ -1,5 +1,6 @@
 package com.example.mvpexample.model
 
+import com.example.mvpexample.data.User
 import com.example.mvpexample.data.UserResponse
 import com.example.mvpexample.io.UserApi
 import io.reactivex.android.schedulers.AndroidSchedulers
@@ -7,17 +8,20 @@ import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
 import java.util.concurrent.TimeUnit
 
-class LoginRepositoryImp(private val userApi: UserApi) : LoginRepository {
+class LoginRepositoryImp(
+    private val userApi: UserApi
+) : LoginRepository {
 
-    val compositeDisposable = CompositeDisposable()
+    private val compositeDisposable: CompositeDisposable = CompositeDisposable()
 
     override fun login(
+        user: User,
         responseSuccess: (UserResponse) -> Unit,
         responseError: (String) -> Unit
     ) {
         compositeDisposable.add(
             userApi
-                .login()
+                .login(user)
                 .delay(3000, TimeUnit.MILLISECONDS)
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
